7a614a98dfb5b1107bb4665b6faf9e64cb90cbed
Muteba-K <makanku2@gmail.com>
2023-08-22 16:06:52 -0700

    Updates

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
  * @format: format.
@@ -87,14 +88,11 @@ return (printed_chars);
 
 }
 
-
-
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-
 void print_buffer(char buffer[], int *buff_ind)
 
 {
